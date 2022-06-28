a62bd2815aca7f2751c8b73a4ad5bc9ebe57e05c
Simone Sada <si.sada93@gmail.com>
2022-06-28 14:08:28 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿void StampaArray(int[] array)
+{
+    Console.WriteLine("\nStampa array:");
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+
+        if(i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+
+    Console.WriteLine("]");
+}
+
+
+
+// Programma principale
+
+int[] array = { 1, 2, 3 };
+
+StampaArray(array);
