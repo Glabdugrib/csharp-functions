6c3fbe7a006cc257da2216a7298b11c088d2a013
Simone Sada <si.sada93@gmail.com>
2022-06-28 14:18:31 +0200

    Complete exercise

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿void StampaArray(int[] array)
 {
-    Console.WriteLine("\nStampa array:");
     Console.Write("[");
 
     for (int i = 0; i < array.Length; i++)
@@ -17,9 +16,57 @@
 }
 
 
+int Quadrato(int num)
+{
+    return num * num;
+}
+
+
+int[] ElevaArrayAlQuadrato(int[] array)
+{
+    int[] arrayQuadrato = new int[array.Length];
+    
+    for(int i = 0; i < array.Length; i++)
+    {
+        arrayQuadrato[i] = Quadrato(array[i]);
+    }
+
+    return arrayQuadrato;
+}
+
+
+int sommaElementiArray(int[] array)
+{
+    int somma = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        somma += array[i];
+    }
+
+    return somma;
+}
+
 
 // Programma principale
 
-int[] array = { 1, 2, 3 };
+int[] array = { 2, 6, 7, 5, 3, 9 };
 
+Console.WriteLine("\nArray originale:");
 StampaArray(array);
+
+//Console.WriteLine(Quadrato(2));
+
+int[] arrayQuadrato = ElevaArrayAlQuadrato(array);
+
+Console.WriteLine("\nArray al quadrato:");
+StampaArray(arrayQuadrato);
+
+Console.WriteLine("\nArray originale:");
+StampaArray(array);
+
+Console.Write("\nSomma array originale: ");
+Console.WriteLine(sommaElementiArray(array));
+
+Console.Write("\nSomma array al quadrato: ");
+Console.WriteLine(sommaElementiArray(arrayQuadrato));
